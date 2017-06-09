e8faa9bdba591c282689e22244e652f7c84a2926
bilxcode <iam@billtanthowijauhari.net>
2017-06-09 12:26:02 +0700

    bug fix

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -55,14 +55,14 @@ class Admin extends CI_Controller {
 	{
 		$id = $this->uri->segment(3);
 		$this->m_content->delete($id);
-		return redirect(base_url('index.php/admin/contentlist'));
+		// return redirect(base_url('index.php/admin/contentlist'));
 	}
 
 	public function contentdelete()
 	{
-		$id = $this->uri->segment(3);
+		$id = $this->uri->segment(4);
 		$this->m_content->delete($id);
-		return redirect(base_url('index.php/admin/contentlist'));
+		// return redirect(base_url('index.php/admin/contentlist'));
 	}
 
 	
