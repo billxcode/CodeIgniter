afa520acaea035eca50937d9a4fb99925b9c6df1
bilxcode <iam@billtanthowijauhari.net>
2017-06-09 12:54:05 +0700

    add home menu

--- application/views/template/v_admin_sidebar.php
+++ application/views/template/v_admin_sidebar.php
@@ -1,6 +1,7 @@
           <div class="well"  style="padding: 8px 0;">
             <ul class="nav nav-list">
               <li class="nav-header">Menu</li>
+              <li><a href="<?php echo base_url('index.php/admin'); ?>">Home</a></li>
               <li><a href="<?php echo base_url('index.php/admin/contentcreate'); ?>">Create</a></li>
               <li><a href="<?php echo base_url('index.php/admin/contentlist'); ?>">List</a></li>
               
